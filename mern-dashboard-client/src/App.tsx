634189ef031a36c43dd068a17eef44bfc176c9ee
vipinsunny <vipinsunny96@gmail.com>
2023-06-28 18:45:25 +0530

    second commit

--- mern-dashboard-client/src/App.tsx
+++ mern-dashboard-client/src/App.tsx
@@ -60,7 +60,7 @@ function App() {
 
             if (profileObj) {
                 const response = await fetch(
-                    "http://localhost:8080/api/v1/users",
+                    "https://refine-dashboard-dcm7.onrender.com/api/v1/users",
                     {
                         method: "POST",
                         headers: { "Content-Type": "application/json" },
@@ -131,7 +131,7 @@ function App() {
             <GlobalStyles styles={{ html: { WebkitFontSmoothing: "auto" } }} />
             <RefineSnackbarProvider>
                 <Refine
-                    dataProvider={dataProvider("http://localhost:8080/api/v1")}
+                    dataProvider={dataProvider("https://refine-dashboard-dcm7.onrender.com/api/v1")}
                     notificationProvider={notificationProvider}
                     ReadyPage={ReadyPage}
                     catchAll={<ErrorComponent />}
